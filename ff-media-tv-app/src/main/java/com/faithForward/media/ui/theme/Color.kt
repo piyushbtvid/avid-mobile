7ae2ed0de831367da3e0f4d805f032ff6ed539fa
Vijay Singh Dhoni <vijaysingh5034@gmail.com>
2025-05-13 17:59:34 +0530

    created Ui for CreatorCard

--- ff-media-tv-app/src/main/java/com/faithForward/media/ui/theme/Color.kt
+++ ff-media-tv-app/src/main/java/com/faithForward/media/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package com.faithForward.media.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
+val text100 = Color(0xFFFFFFFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
@@ -11,6 +12,7 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val focusedMainColor = Color(0xFFB87333)
+val creatorShadowColor = Color(0xB0B87333)
 val unFocusMainColor = Color(0xFFF4EFE6)
 val textFocusedMainColor = Color(0xFFEAE3C9)
 val textUnFocusColor = Color(0xFFB5883B)
