99ddc01f7a37da0c4d1253ac16d0daf5efddb0ac
Vijay Singh Dhoni <vijaysingh5034@gmail.com>
2025-05-21 18:07:16 +0530

    added new home api and removed the /build files from git tracking

--- ff-media-tv-app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_faithForward_media_App.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.faithForward.media.App"
-)
-public final class _com_faithForward_media_App {
-}
