f5ef46c300d3d0ff68d354df79b2bdbd5dd1c328
Vijaysingh <vijaysingh5034@gmail.com>
2025-07-14 18:03:50 +0530

    added window manger keep screen on in main Activity

--- ff-media-tv-app/src/main/java/com/faithForward/media/activity/MainActivity.kt
+++ ff-media-tv-app/src/main/java/com/faithForward/media/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.faithForward.media.activity
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -176,6 +177,16 @@ class MainActivity : ComponentActivity() {
         }
 
         return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 
 }
