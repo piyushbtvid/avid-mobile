324299aa546aa281d0289dafbc1892830d19dac0
Vijaysingh <vijaysingh5034@gmail.com>
2025-08-06 19:16:51 +0530

    updated verson code

--- ff-media-tv-app/build.gradle.kts
+++ ff-media-tv-app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.faithForward.media"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "3.3"
+        versionCode = 7
+        versionName = "3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
